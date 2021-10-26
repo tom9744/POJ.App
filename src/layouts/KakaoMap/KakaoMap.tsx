08de8f29ebd9226aca398e1e75d095e9727522a4
Junyoung <tom9744@gmail.com>
2021-10-26 23:56:46 +0900

    Kakao Map API 연동

---
+++ src/layouts/KakaoMap/KakaoMap.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from "react";
+import styles from "./KakaoMap.module.css";
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+const KAKAO_MAP_CONFIG = {
+  center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+  level: 10,
+};
+
+function KakaoMap() {
+  // Reference of the DOM where the map will be displayed.
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    // Load the kakao API map into the DOM.
+    new window.kakao.maps.Map(mapContainer.current, KAKAO_MAP_CONFIG);
+
+    return () => {};
+  }, []);
+
+  return <div className={styles.map} ref={mapContainer}></div>;
+}
+
+export default KakaoMap;
