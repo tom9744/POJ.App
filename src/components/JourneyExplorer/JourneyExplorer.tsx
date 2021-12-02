b330c190568cbe5d4fdb913d9aea56ad0d772255
Junyoung <tom9744@gmail.com>
2021-12-03 00:04:45 +0900

    서버가 응답하지 않거나, 로딩중인 경우 JourneyExplorer 상태를 확인할 수 있도록 변경

--- src/components/JourneyExplorer/JourneyExplorer.tsx
+++ src/components/JourneyExplorer/JourneyExplorer.tsx
@@ -18,6 +18,10 @@ type ExplorerAction =
   | { type: "TOGGLE_JOURNEY_FORM" }
   | { type: "SET_SHOW_JOURNEY_DETAIL"; props: boolean }
   | { type: "SET_SHOW_LOADING"; props: boolean }
+  | {
+      type: "SET_SHOW_ERROR";
+      props: { isError: boolean; errorMessage: string };
+    }
   | { type: "FETCH_JOURNEY"; props: ProcessedJourney[] }
   | { type: "APPEND_JOURNEY"; props: ProcessedJourney[] }
   | { type: "DELETE_JOURNEY"; props: ProcessedJourney }
@@ -26,9 +30,11 @@ type ExplorerAction =
 interface ExplorerState {
   showJourneyForm: boolean;
   showJourneyDatail: boolean;
+  showError: boolean;
   showLoading: boolean;
   journeys: ProcessedJourney[];
   selectedJourney: ProcessedJourney | null;
+  errorMessage: string;
 }
 
 const reducer = (
@@ -42,6 +48,12 @@ const reducer = (
       return { ...state, showJourneyDatail: action.props };
     case "SET_SHOW_LOADING":
       return { ...state, showLoading: action.props };
+    case "SET_SHOW_ERROR":
+      return {
+        ...state,
+        showError: action.props.isError,
+        errorMessage: action.props.errorMessage,
+      };
     case "FETCH_JOURNEY":
       return { ...state, journeys: action.props };
     case "DELETE_JOURNEY":
@@ -67,8 +79,10 @@ function JourneyExplorer(props: JourneyExplorerProps) {
     showJourneyForm: false,
     showJourneyDatail: false,
     showLoading: false,
+    showError: false,
     journeys: [],
     selectedJourney: null,
+    errorMessage: "",
   });
 
   // NOTE: Cannot use async/await in useEffect Hook.
@@ -76,18 +90,26 @@ function JourneyExplorer(props: JourneyExplorerProps) {
     if (!props.isActive) return;
 
     dispatch({ type: "SET_SHOW_LOADING", props: true });
+    dispatch({
+      type: "SET_SHOW_ERROR",
+      props: { isError: false, errorMessage: "" },
+    });
 
     fetchJourneys()
       .then((journeys) => {
         dispatch({ type: "FETCH_JOURNEY", props: processJourneys(journeys) });
       })
       .catch((error) => {
-        // TODO: Should improve the error handling logic to enhance UX.
-        if (error instanceof HttpError) {
-          alert("Could not fetch data from the server. Please try later.");
-        } else {
-          alert("Could not reach out to the server. Please try later.");
-        }
+        dispatch({
+          type: "SET_SHOW_ERROR",
+          props: {
+            isError: true,
+            errorMessage:
+              error instanceof HttpError
+                ? "서버와 통신하는 도중에 오류가 발생했습니다."
+                : "서버가 응답하지 않습니다. 잠시 후 다시 시도해주세요.",
+          },
+        });
       })
       .finally(() => {
         dispatch({ type: "SET_SHOW_LOADING", props: false });
@@ -132,12 +154,19 @@ function JourneyExplorer(props: JourneyExplorerProps) {
           {
             type: "block",
             textContent: "기록 남기기",
+            isDisabled: state.showError || state.showLoading,
             handler: toggleJourneyForm,
           },
         ]}
       ></ExplorerHeader>
 
-      {state.showLoading && <div className={classes.loading}>Loading...</div>}
+      {state.showLoading && (
+        <div className={classes.loading}>불러오는 중...</div>
+      )}
+
+      {state.showError && (
+        <div className={classes.error}>{state.errorMessage}</div>
+      )}
 
       <JourneyList
         journeys={state.journeys}
