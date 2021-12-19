2fba01624e06d2a581a293835a78eb214409a274
Junyoung <tom9744@gmail.com>
2021-12-19 18:10:44 +0900

    시작일로부터 경과된 기간을 표현하기 위한 데이터 타입 추가

--- src/components/JourneyExplorer/JourneyService.ts
+++ src/components/JourneyExplorer/JourneyService.ts
@@ -1,4 +1,25 @@
-import { RawJourney, ProcessedJourney, RawPhoto } from "./Journey.interface";
+import {
+  RawJourney,
+  ProcessedJourney,
+  RawPhoto,
+  ElapsedDate,
+} from "./Journey.interface";
+
+const calculateElapsedDate = (originDate: string): ElapsedDate => {
+  const originTime = new Date(originDate).getTime();
+  const currentTime = new Date().getTime();
+  const elapsedTime = currentTime - originTime;
+
+  const elapsedMinutes = elapsedTime / (1000 * 60);
+  const elapsedHours = elapsedMinutes / 60;
+  const elapsedDays = elapsedHours / 24;
+
+  return new ElapsedDate(
+    Math.floor(elapsedMinutes),
+    Math.floor(elapsedHours),
+    Math.floor(elapsedDays)
+  );
+};
 
 const modifyDateString = (date: string): string => {
   return date.substring(0, 10).replace(/-/g, ".");
@@ -24,21 +45,21 @@ export const processJourneys = (journeys: RawJourney[]): ProcessedJourney[] => {
     return [];
   }
 
-  const processedJourneys = journeys
-    .map((journey): RawJourney => {
-      return {
-        ...journey,
-        photos: modifyImagePath(journey.photos),
-        startDate: modifyDateString(journey.startDate),
-        endDate: modifyDateString(journey.endDate),
-      };
-    })
-    .map((journey): ProcessedJourney => {
-      return {
-        ...journey,
-        thumbNailPath: journey.photos?.length > 0 ? journey.photos[0].path : ``,
-      };
-    });
+  const processedJourneys = journeys.map((journey): ProcessedJourney => {
+    const modifiedPhotos = modifyImagePath(journey.photos);
+    const modifiedStartDate = modifyDateString(journey.startDate);
+    const modifiedEndDate = modifyDateString(journey.endDate);
+    const elapsedDate = calculateElapsedDate(journey.startDate);
+
+    return {
+      ...journey,
+      photos: modifyImagePath(journey.photos),
+      startDate: modifiedStartDate,
+      endDate: modifiedEndDate,
+      elapsedDate,
+      thumbNailPath: modifiedPhotos.length > 0 ? modifiedPhotos[0].path : ``,
+    };
+  });
 
   return processedJourneys;
 };
