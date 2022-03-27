cd6d7fd5b0b114ff949c8e2970030dc9776f6822
Junyoung Yang <tom9744@gmail.com>
2022-03-27 10:16:33 +0900

    useJourneyList Hook 추가

--- src/hooks/useHttp.ts
+++ src/hooks/useHttp.ts
@@ -1,6 +1,6 @@
 import { useCallback, useReducer } from "react";
 
-export class HttpError extends Error {
+class HttpError extends Error {
   constructor(public statusCode: number, public message: string) {
     super(message);
   }
@@ -17,15 +17,9 @@ interface RequestState {
   errorMessage: string;
 }
 
-type RequestAction =
-  | { type: "LOADING" }
-  | { type: "SUCCSESS" }
-  | { type: "ERROR"; message: string };
+type RequestAction = { type: "LOADING" } | { type: "SUCCSESS" } | { type: "ERROR"; message: string };
 
-const requestReducer = (
-  state: RequestState,
-  action: RequestAction
-): RequestState => {
+const requestReducer = (state: RequestState, action: RequestAction): RequestState => {
   switch (action.type) {
     case "LOADING":
       return { showError: false, showLoading: true, errorMessage: "" };
@@ -52,41 +46,35 @@ const useHttp = <T>(): {
     errorMessage: "",
   });
 
-  const sendRequest = useCallback(
-    async ({ url, options }: FetchConfiguration): Promise<T> => {
-      dispatchRequestAction({ type: "LOADING" });
+  const sendRequest = useCallback(async ({ url, options }: FetchConfiguration): Promise<T> => {
+    dispatchRequestAction({ type: "LOADING" });
 
-      try {
-        const response = await fetch(url, options);
+    try {
+      const response = await fetch(url, options);
 
-        if (!response.ok) {
-          const errorData = new HttpError(response.status, response.statusText);
-          throw errorData;
-        }
-
-        dispatchRequestAction({ type: "SUCCSESS" });
-
-        return options?.method !== "DELETE" && options?.method !== "PATCH"
-          ? await response.json()
-          : null;
-      } catch (error) {
-        const errorMessage =
-          error instanceof HttpError
-            ? "서버와 통신하는 도중에 오류가 발생했습니다."
-            : "서버가 응답하지 않습니다. 잠시 후 다시 시도해주세요.";
-
-        dispatchRequestAction({ type: "ERROR", message: errorMessage });
-
-        throw error;
+      if (!response.ok) {
+        const errorData = new HttpError(response.status, response.statusText);
+        throw errorData;
       }
-    },
-    []
-  );
+
+      dispatchRequestAction({ type: "SUCCSESS" });
+
+      return options?.method !== "DELETE" && options?.method !== "PATCH" ? await response.json() : null;
+    } catch (error) {
+      const errorMessage = error instanceof HttpError ? "서버와 통신하는 도중에 오류가 발생했습니다." : "서버가 응답하지 않습니다. 잠시 후 다시 시도해주세요.";
+
+      dispatchRequestAction({ type: "ERROR", message: errorMessage });
+
+      throw error;
+    }
+  }, []);
 
   return {
     requestState,
     sendRequest,
   };
 };
+
+export const BASE_URL = "https://var-resa.link";
 
 export default useHttp;
---
+++ src/pages/JourneyList/hooks/useJourneyList.ts
@@ -0,0 +1,120 @@
+import { useCallback, useEffect, useState } from "react";
+import { IJourneyData, IPhotoData } from "../JourneyData.model";
+import useHttp, { BASE_URL } from "../../../hooks/useHttp";
+
+export interface IElapsedDate {
+  elapsedTime: number;
+  toString(): string;
+}
+
+class ElapsedDate implements IElapsedDate {
+  private _elapsedTime: number;
+
+  constructor(originDate: string) {
+    const originTime = new Date(originDate).getTime();
+    const currentTime = new Date().getTime();
+
+    this._elapsedTime = currentTime - originTime;
+  }
+
+  get elapsedTime(): number {
+    return this._elapsedTime;
+  }
+
+  toString(): string {
+    const elapsedMinutes = Math.floor(this._elapsedTime / (1000 * 60));
+    const elapsedHours = Math.floor(elapsedMinutes / 60);
+    const elapsedDays = Math.floor(elapsedHours / 24);
+
+    if (elapsedDays > 0) {
+      return `${elapsedDays}일 전`;
+    } else if (elapsedHours > 0) {
+      return `${elapsedHours}시간 전`;
+    } else if (elapsedMinutes > 0) {
+      return `${elapsedMinutes}분 전`;
+    } else {
+      return "방금 전";
+    }
+  }
+}
+
+export interface IJoureny {
+  id: number;
+  title: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+  elapsedDate: IElapsedDate;
+  photos: IPhotoData[];
+}
+
+class Journey implements IJoureny {
+  private _id: number;
+  get id(): number {
+    return this._id;
+  }
+
+  private _title: string;
+  get title(): string {
+    return this._title;
+  }
+
+  private _description: string;
+  get description(): string {
+    return this._description;
+  }
+
+  private _startDate: string;
+  get startDate(): string {
+    return this._startDate;
+  }
+
+  private _endDate: string;
+  get endDate(): string {
+    return this._endDate;
+  }
+
+  private _elapsedDate: IElapsedDate;
+  get elapsedDate(): IElapsedDate {
+    return this._elapsedDate;
+  }
+
+  private _photos: IPhotoData[];
+  get photos(): IPhotoData[] {
+    return [...this._photos];
+  }
+
+  constructor(arg: IJourneyData) {
+    this._id = arg.id;
+    this._title = arg.title;
+    this._description = arg.description;
+    this._startDate = arg.startDate;
+    this._endDate = arg.endDate;
+    this._elapsedDate = new ElapsedDate(arg.startDate);
+    this._photos = arg.photos;
+  }
+}
+
+export const useJourneyList = () => {
+  const [journeyList, setJourneyList] = useState<IJoureny[]>([]);
+  const { requestState, sendRequest } = useHttp<IJourneyData[]>();
+
+  const sortByElapsedDate = useCallback((itemA: IJoureny, itemB: IJoureny) => {
+    return itemA.elapsedDate.elapsedTime - itemB.elapsedDate.elapsedTime;
+  }, []);
+
+  useEffect(() => {
+    sendRequest({ url: `${BASE_URL}/journeys` })
+      .then((jourenyDataList) => {
+        const journeyList = jourenyDataList.map((joureny) => new Journey(joureny)).sort(sortByElapsedDate);
+
+        setJourneyList(journeyList);
+      })
+      .catch((error) => console.error(error));
+  }, [sendRequest, sortByElapsedDate]);
+
+  return {
+    requestState,
+    journeyList,
+  };
+};
