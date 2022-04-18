0e211dc916c3c568df1f35f0765c9946cf658cbf
Junyoung Yang <tom9744@gmail.com>
2022-04-19 01:21:01 +0900

    간단한 검색 기능 추가

--- src/pages/JourneyList/JourneyList.tsx
+++ src/pages/JourneyList/JourneyList.tsx
@@ -1,6 +1,7 @@
+import { useCallback, useEffect, useState } from "react";
 import Footer from "../../components/UI/Footer/Footer";
 import Header from "../../components/UI/Header/Header";
-import { useJourneyList } from "./hooks/useJourneyList";
+import { IJoureny, useJourneyList } from "./hooks/useJourneyList";
 import classes from "./JourneyList.module.scss";
 import JourneyListItem from "./JourneyListItem/JourneyListItem";
 import SearchBar from "./SearchBar/SearchBar";
@@ -8,14 +9,31 @@ import SearchBar from "./SearchBar/SearchBar";
 function JourneyList() {
   const { journeyList } = useJourneyList();
 
+  const [searchResult, setSearchResult] = useState<IJoureny[]>([]);
+  const [keyword, setKeyword] = useState<string>("");
+
+  const searchHandler = useCallback((keyword: string) => {
+    setKeyword(keyword);
+  }, []);
+
+  useEffect((): void => {
+    if (!keyword) {
+      setSearchResult(journeyList);
+      return;
+    }
+    // NOTE: 제목 또는 설명에 키워드가 포함되어 있는 데이터를 추출합니다.
+    const searchResult = journeyList.filter(({ title, description }) => title.includes(keyword) || description.includes(keyword));
+    setSearchResult(searchResult);
+  }, [keyword, journeyList]);
+
   return (
     <article className={classes["journey-list-container"]}>
       <Header></Header>
 
-      <SearchBar></SearchBar>
+      <SearchBar onSearch={searchHandler}></SearchBar>
 
       <ul className={classes["journey-list-wrapper"]}>
-        {journeyList.map((journey) => {
+        {searchResult.map((journey) => {
           return <JourneyListItem key={journey.id} journey={journey}></JourneyListItem>;
         })}
       </ul>
--- src/pages/JourneyList/SearchBar/SearchBar.tsx
+++ src/pages/JourneyList/SearchBar/SearchBar.tsx
@@ -1,11 +1,33 @@
 import classes from "./SearchBar.module.scss";
 import { FaSearch } from "react-icons/fa";
+import { useCallback, useState } from "react";
 
-function SearchBar() {
+type SearchBarProp = {
+  onSearch: (keyword: string) => void;
+};
+
+function SearchBar({ onSearch }: SearchBarProp) {
+  const [value, setValue] = useState<string>("");
+
+  const submitHandler = useCallback(
+    (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      onSearch(value);
+    },
+    [value, onSearch]
+  );
+
+  const inputHandler = useCallback((event: React.FormEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+
+    setValue(target.value);
+  }, []);
+
   return (
-    <form className={classes["search-bar-wrapper"]}>
+    <form className={classes["search-bar-wrapper"]} onSubmit={submitHandler}>
       <FaSearch className={`${classes["search-icon"]}`} />
-      <input className={classes["search-bar"]} type="text" />
+      <input className={classes["search-bar"]} type="text" value={value} onInput={inputHandler} />
       <button className={classes["search-button"]}>검색</button>
     </form>
   );
